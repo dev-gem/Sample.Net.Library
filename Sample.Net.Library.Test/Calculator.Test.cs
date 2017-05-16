236d3a6931aad9196ab5cb959e89aff68c8c0b74
Lou Parslow <lou.parslow@gmail.com>
2017-05-16 17:49:24 -0600

    'all'

---
+++ Sample.Net.Library.Test/Calculator.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sample.Net.Library.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public static void Add()
+        {
+            Assert.AreEqual(10.0, Calculator.Add(5, 5));
+        }
+    }
+}
---
+++ Sample.Net.Library/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace Sample.Net.Library
+{
+    public static class Calculator
+    {
+        public static double Add(double a, double b) { return a + b; }
+    }
+}
